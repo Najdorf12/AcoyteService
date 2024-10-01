515a9187046f6d8f9188a1c6b516047957862f28
Najdorf12 <agustin.morro@gmail.com>
2024-10-01 13:45:27 -0300

    add styles

--- src/pages/Contact.jsx
+++ src/pages/Contact.jsx
@@ -11,14 +11,14 @@ const Contact = () => {
           <article >
             <h2
               style={{ animation: "appear 2s ease-out" }}
-              class="text-5xl text-start font-semibold font-title2  text-white  pl-3"
+              class="text-5xl text-start font-semibold font-title2  text-white  pl-5"
             >
               Contacto
             </h2>
-            <div className="w-[80%] h-[2px] bg-blue-500 mt-3"></div>
+            <div className="w-[80%] h-[2px] bg-pastel mt-3"></div>
             <p
               style={{ animation: "appear 3s ease-out" }}
-              className="text-stone-400 mt-6 text-sm  font-title font-medium text-start text-balance xl:text-lg pl-3"
+              className="text-zinc-400 mt-5 text-sm  font-title font-medium text-start text-balance xl:text-lg pl-5"
             >
               Lorem ipsum dolor sit, amet consectetur adipisicing elit. Quae
               asperiores dolores nisi. Soluta, quidem. Veritatis eveniet magnam
@@ -42,7 +42,7 @@ const Contact = () => {
                   type="email"
                 />
                 <label
-                  className=" absolute left-0 -top-3.5  text-sm transition-all peer-placeholder-shown:text-lg peer-placeholder-shown:text-white peer-placeholder-shown:top-2 peer-focus:-top-3.5 peer-focus:text-stone-500 peer-focus:text-base font-normal lg:peer-placeholder-shown:text-stone-600 2xl:mt-6"
+                  className=" absolute left-0 -top-3.5  text-sm transition-all peer-placeholder-shown:text-lg peer-placeholder-shown:text-pastel peer-placeholder-shown:top-2 peer-focus:-top-3.5 peer-focus:text-stone-500 peer-focus:text-base font-normal lg:peer-placeholder-shown:text-stone-600 2xl:mt-6"
                   for="email"
                 >
                   Email 
@@ -52,17 +52,17 @@ const Contact = () => {
                 <input
                   autoComplete="off"
                   placeholder="Password"
-                  className="peer h-10 font-title w-full border-b-2 border-pastel text-white lg:text-stone-600 bg-transparent placeholder-transparent focus:outline-none focus:border-blue-600"
+                  className="peer h-10 font-title w-full border-b-2 border-pastel text-white  bg-transparent placeholder-transparent focus:outline-none focus:border-blue-600"
                   required=""
                   id="password"
                   name="password"
                 />
-                <label className="absolute left-0 -top-3.5 text-gray-800 text-sm transition-all peer-placeholder-shown:text-lg peer-placeholder-shown:text-white peer-placeholder-shown:top-2 peer-focus:-top-3.5 peer-focus:text-stone-500 peer-focus:text-base font-normal lg:peer-placeholder-shown:text-stone-600 ">
+                <label className="absolute left-0 -top-3.5 text-gray-800 text-sm transition-all peer-placeholder-shown:text-lg peer-placeholder-shown:text-pastel peer-placeholder-shown:top-2 peer-focus:-top-3.5 peer-focus:text-stone-500 peer-focus:text-base font-normal lg:peer-placeholder-shown:text-stone-600 ">
                   WhatsApp
                 </label>
               </div>
              
-              <textarea id="box-glass" placeholder="Escribe tu consulta aquí ..." name="textarea" className="w-full h-[150px] bg-transparent  pt-2 px-3 rounded-2xl border border-pastel text-white  font-title placeholder-pastel "></textarea>
+              <textarea id="box-glass" placeholder="Escribe tu consulta aquí ..." name="textarea" className="w-full h-[150px] bg-transparent  pt-2 px-3 rounded-2xl border border-pastel text-white  font-title placeholder-pastel focus:border-blue-600"></textarea>
               <button
               id="box-glass"
                 className="w-full border border-pastel py-1 px-4  rounded-full shadow-lg text-white font-semibold transition duration-200 text-lg font-title "
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -6,17 +6,18 @@ const Home = () => {
     <>
       <section className="z-50 w-full h-screen relative">
         <nav className="w-full  flex justify-evenly text-zinc-800 pt-2 font-text lg:justify-center lg:gap-12">
+          <button className="border border-blue-00 px-6 rounded-xl  flex justify-center items-center py-[2px]  shadow-md shadow-zinc-800 lg:px-9">
+            Button
+          </button>
+          <button className="border border-pastel px-6 rounded-xl bg-zinc-800 flex justify-center items-center py-[2px] text-white shadow-md shadow-zinc-600 lg:px-9">
+            Button
+          </button>
+
           <button className=" border border-pastel px-6 rounded-xl bg-zinc-800 flex justify-center items-center py-[2px] text-white shadow-md shadow-zinc-600 lg:px-9">
             Contacto
           </button>
-          <button className="border border-blue-00 px-6 rounded-xl  flex justify-center items-center py-[2px]  shadow-md shadow-zinc-800 lg:px-9">
-            Lorem
-          </button>
-          <button className="border border-blue-00 px-6 rounded-xl  flex justify-center items-center py-[2px]  shadow-md shadow-zinc-800 lg:px-9">
-            Lorem
-          </button>
         </nav>
-        <article className="z-50 px-3 flex flex-col text-balance text-center justify-center items-center mt-10">
+        <article className="z-50 px-3 flex flex-col text-balance text-center justify-center items-center mt-7">
           <h1 className="text-6xl font-title2 font-bold text-zinc-800 z-50 leading-[3.5rem] xl:text-8xl 2xl:text-9xl">
             ACOYTE SERVICE
           </h1>
@@ -26,14 +27,13 @@ const Home = () => {
           </h5>
         </article>
 
-        <div className="w-full absolute bottom-2 flex justify-between text-balance">
-          <div className="h-[150px] w-[45%] max-w-[350px] bg-gradient-to-tr from-zinc-800 via-blue-800 to-trasparent p-3 text-base font-title text-white rounded-r-2xl lg:h-[200px] lg:w-[25%]">
-            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit</p>
+        <div className="w-full absolute bottom-6 flex justify-between text-balance">
+          <div className="h-[120px] w-[45%] max-w-[350px] bg-gradient-to-tr from-zinc-800 via-blue-800 to-trasparent p-3 text-base font-title text-white rounded-r-2xl lg:h-[200px] lg:w-[25%]">
+            <p>Somo random text or link. Lorem ipsum</p>
             <div className="w-[50%] h-[2.5px] bg-white mt-4 relative flex items-center justify-center">
               <i className="bx bx-right-arrow-alt absolute -right-2 text-2xl mt-[.98px]"></i>
             </div>
           </div>
-          <div className="h-[150px]  bg-blue-900"></div>
         </div>
       </section>
     </>
